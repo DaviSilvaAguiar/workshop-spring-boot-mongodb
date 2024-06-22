8ed729ba2dc5e4d166ae0ed0fa289e48f7d2fb0f
Davi Aguiar <daviagui13@gmail.com>
2024-06-22 11:42:30 -0300

    Obtendo um usuário por ID

--- src/main/java/com/cursoudemy/workshopmongo/services/UserService.java
+++ src/main/java/com/cursoudemy/workshopmongo/services/UserService.java
@@ -1,21 +1,28 @@
 package com.cursoudemy.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cursoudemy.workshopmongo.domain.User;
 import com.cursoudemy.workshopmongo.repository.UserRepository;
+import com.cursoudemy.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
 }
