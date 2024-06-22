c80582d6db6f33cf24e0a34f189e140f9d2d578b
Davi Aguiar <daviagui13@gmail.com>
2024-06-22 13:40:33 -0300

    Criando entity Post com User alinhado

---
+++ src/main/java/com/cursoudemy/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.cursoudemy.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cursoudemy.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+	
+	
+}
